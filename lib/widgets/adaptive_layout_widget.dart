b94970829d7e32d2dc36f910c1a975af010fc48f
mohamed yasser <medoyasser511@gmail.com>
2025-01-19 21:16:39 +0200

    create adaptive layout

---
+++ lib/widgets/adaptive_layout_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AdaptiveLayout extends StatelessWidget {
+  const AdaptiveLayout({super.key, required this.mobileLayout, required this.tabletLayout, required this.disktopLayout});
+
+final WidgetBuilder mobileLayout , tabletLayout , disktopLayout;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: LayoutBuilder(builder: (context, constraints) {
+        if (constraints.maxWidth < 600) {
+          return mobileLayout(context);
+        } else if (constraints.maxWidth < 900) {
+          return tabletLayout(context);
+        } else {
+          return disktopLayout(context);
+        }
+      }),
+    );
+  }
+}
