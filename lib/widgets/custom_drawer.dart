fac87e53213ec9871ec056ee09f017f4f9fa80a9
mohamed yasser <medoyasser511@gmail.com>
2025-01-21 17:56:16 +0200

    refactor drawer items list view

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,43 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dash_board/models/drawer_item_model.dart';
 import 'package:responsive_dash_board/utils/app_images.dart';
-import 'package:responsive_dash_board/widgets/drawer_item.dart';
+import 'package:responsive_dash_board/widgets/drawer_items_list_view.dart';
 import 'package:responsive_dash_board/widgets/user_info_list_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
 
-  static const List<DrawerItemModel> Items = [
-    DrawerItemModel(title: 'Dashboard', image: Assets.imagesDashboard),
-    DrawerItemModel(title: 'My Transaction', image: Assets.imagesMyTransctions),
-    DrawerItemModel(title: 'Statistics', image: Assets.imagesStatistics),
-    DrawerItemModel(title: 'Wallet Account', image: Assets.imagesWalletAccount),
-    DrawerItemModel(title: 'My Investments', image: Assets.imagesMyInvestments),
-  ];
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Column(
+      child: const Column(
         children: [
-          const UserInfoListTile(
+          UserInfoListTile(
               image: Assets.imagesAvatar3,
               title: 'Lekan Okeowo',
               subtitle: 'demo@gmail.com'),
-          const SizedBox(
+          SizedBox(
             height: 8,
           ),
-          ListView.builder(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: Items.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: DrawerItem(drawerItemModel: Items[index]),
-              );
-            },
-          ),
+          DrawerItemsListView(),
         ],
       ),
     );
---
+++ lib/widgets/drawer_items_list_view.dart
@@ -0,0 +1,34 @@
+
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/drawer_item_model.dart';
+import 'package:responsive_dash_board/utils/app_images.dart';
+import 'package:responsive_dash_board/widgets/drawer_item.dart';
+
+class DrawerItemsListView extends StatelessWidget {
+  const DrawerItemsListView({
+    super.key,
+  });
+
+ static const List<DrawerItemModel> Items = [
+    DrawerItemModel(title: 'Dashboard', image: Assets.imagesDashboard),
+    DrawerItemModel(title: 'My Transaction', image: Assets.imagesMyTransctions),
+    DrawerItemModel(title: 'Statistics', image: Assets.imagesStatistics),
+    DrawerItemModel(title: 'Wallet Account', image: Assets.imagesWalletAccount),
+    DrawerItemModel(title: 'My Investments', image: Assets.imagesMyInvestments),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: Items.length,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.only(top: 20.0),
+          child: DrawerItem(drawerItemModel: Items[index]),
+        );
+      },
+    );
+  }
+}
