1b9ff1a7bc5e08049e7d59e43d42a6aa8927dfdf
mohamed yasser <medoyasser511@gmail.com>
2025-01-26 18:55:28 +0200

    trigger list view and solve height and width exception

--- lib/widgets/latest_transction_list_view.dart
+++ lib/widgets/latest_transction_list_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/models/user_info_model.dart';
+import 'package:responsive_dash_board/utils/app_images.dart';
 import 'package:responsive_dash_board/widgets/user_info_list_tile.dart';
 
 class LatestTransctionListView extends StatelessWidget {
@@ -8,23 +9,27 @@ class LatestTransctionListView extends StatelessWidget {
     UserInfoModel(
         title: 'Madrani Andi',
         subtite: 'Madraniadi20@gmail',
-        image: 'Assests.imagesAvatar1'),
+        image: Assets.imagesAvatar1),
     UserInfoModel(
         title: 'Josua Nunito',
         subtite: 'Josh Nunito@gmail.com',
-        image: 'Assests.imagesAvatar2'),
+        image: Assets.imagesAvatar2),
     UserInfoModel(
         title: 'Madrani Andi',
         subtite: 'Madraniadi20@gmail',
-        image: 'Assests.imagesAvatar3'),
+        image: Assets.imagesAvatar3),
   ];
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: items.length,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return UserInfoListTile(userInfoModel: items[index]);
-        });
+    return SizedBox(
+      height: 80,
+      child: ListView.builder(
+          itemCount: items.length,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return IntrinsicWidth(
+                child: UserInfoListTile(userInfoModel: items[index]));
+          }),
+    );
   }
 }
--- lib/widgets/quick_invoice.dart
+++ lib/widgets/quick_invoice.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/Custom_background_container.dart';
+import 'package:responsive_dash_board/widgets/latest_transction_list_view.dart';
 import 'package:responsive_dash_board/widgets/quick_invoice_header.dart';
 
 class QuickInvoice extends StatelessWidget {
@@ -9,7 +10,10 @@ class QuickInvoice extends StatelessWidget {
   Widget build(BuildContext context) {
     return const CustyomBackgroundContainer(
         child: Column(
-      children: [QuickInvoiceHeader()],
+      children: [
+        QuickInvoiceHeader(),
+        LatestTransctionListView(),
+      ],
     ));
   }
 }
