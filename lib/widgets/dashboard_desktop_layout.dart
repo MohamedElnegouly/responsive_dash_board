cdbd43862e12a8ddc265c418748e1a21e18012b2
mohamed yasser <medoyasser511@gmail.com>
2025-01-27 19:27:44 +0200

    refactor section 2

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dash_board/widgets/All_expenses.dart';
+import 'package:responsive_dash_board/widgets/All_expenses_and_quick_invoice.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
-import 'package:responsive_dash_board/widgets/quick_invoice.dart';
+
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -14,18 +14,10 @@ class DashboardDesktopLayout extends StatelessWidget {
         SizedBox(width: 32),
         Expanded(
           flex: 2,
-          child: Column(
-            children: [
-              SizedBox(
-                height: 40,
-              ),
-              AllExpenses(),
-              SizedBox(height: 24),
-              QuickInvoice(),
-            ],
-          ),
+          child: AllExpensesAndQuickinvoice(),
         ),
       ],
     );
   }
 }
+
