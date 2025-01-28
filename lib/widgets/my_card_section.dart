1116a471343c538381ef1871a06ecb6410a92d9a
mohamed yasser <medoyasser511@gmail.com>
2025-01-28 21:22:41 +0200

    trigger dots indicator with page view

--- lib/widgets/my_card_section.dart
+++ lib/widgets/my_card_section.dart
@@ -3,25 +3,46 @@ import 'package:responsive_dash_board/utils/app_styles.dart';
 import 'package:responsive_dash_board/widgets/dots_indicator.dart';
 import 'package:responsive_dash_board/widgets/my_cards_page_view.dart';
 
-class MyCardSection extends StatelessWidget {
+class MyCardSection extends StatefulWidget {
   const MyCardSection({super.key});
 
   @override
+  State<MyCardSection> createState() => _MyCardSectionState();
+}
+
+class _MyCardSectionState extends State<MyCardSection> {
+  late PageController pageController;
+  int currentPageIndex = 0;
+  @override
+  void initState() {
+    pageController = PageController();
+    pageController.addListener(() {
+      currentPageIndex = pageController.page!.round();
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SizedBox(
+        const SizedBox(
           width: 420,
           child: Text(
             'My card ',
             style: AppStyles.styleSemiBold20,
           ),
         ),
-        SizedBox(height: 20),
-        MyCardsPageView(),
-        SizedBox(height: 20),
-        DotsIndicator(),
+        const SizedBox(height: 20),
+        MyCardsPageView(
+          pageController: pageController,
+        ),
+        const SizedBox(height: 20),
+        DotsIndicator(
+          currentPageIndex: currentPageIndex,
+        ),
       ],
     );
   }
